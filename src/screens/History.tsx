f6ff9805f4dae1f731df2fb2693a67d0811d70ec
isabellebryans <bryansi@tcd.ie>
2023-11-28 17:21:15 +0000

    reading data points correctly

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -5,6 +5,7 @@ import Home from "./Home";
 import { useNavigation } from "@react-navigation/native";
 import React, {useState, useEffect} from 'react';
 import firestore from '@react-native-firebase/firestore';
+import { Timestamp } from '@firebase/firestore-types';
 import { format, subDays } from 'date-fns';
 import {
     LineChart,
@@ -15,11 +16,16 @@ import {
      StackedBarChart
    } from "react-native-chart-kit";
 
+interface DataPoint {
+x: string; // Assuming x is a string representing the timestamp
+y: number; // Assuming y is a number representing the air quality value
+}
+
 function ViewHistory(){
     const navigation = useNavigation()
     const currentTimestamp = useState(null);
     const [OldTimestamp, setOldTimestamp] = useState(null);
-    const [dataPoints, setDataPoints] = useState([]);
+    const [dataPoints, setDataPoints] = useState<DataPoint[]>([]);
     const getCurrentTimestamp = (): number => {
         return Math.floor(new Date().getTime() / 1000);
       };
@@ -27,28 +33,32 @@ function ViewHistory(){
 
     const getReadings = async ()=>{
         try {
-            // Create a reference to the "monitors" collection
-            const airQualityRef = firestore().collection('Nearby_air_quality');
-        
+            console.log('Trying to get readings');
             // Calculate the timestamp for 24 hours ago
             const twentyFourHoursAgo = subDays(new Date(), 1).getTime() / 1000;
+            // Create a reference to the "monitors" collection
+            const airQualityRef = await firestore().collection('Nearby_air_quality').get();
+    
 
             // Perform a Firestore query to get items within the last 24 hours
-            const querySnapshot = await airQualityRef.where('timestamp', '>=', twentyFourHoursAgo).get();
+            console.log("got the goods. Time 24 hours ago is: ", twentyFourHoursAgo)
+            //const querySnapshot = await airQualityRef.where('timestamp.toDate()', '>=', twentyFourHoursAgo).get();
             const newDataPoints = [];
-            querySnapshot.forEach((doc) => {
+            console.log("Got the goods.")
+            airQualityRef.forEach((doc) => {
+                console.log('Reading is', doc);
                 const data = doc.data();
-                newDataPoints.push({
-                x: format(new Date(data.timestamp * 1000), 'HH:mm'), // Format timestamp as desired
-                y: data.pm25Value, // Use the appropriate field from your data
-                });
+                console.log('Timestamp is ', data.timestamp.seconds);
+                if(data.timestamp.seconds >= twentyFourHoursAgo){
+                    newDataPoints.push({
+                        x: format(new Date(data.timestamp.seconds* 1000), 'HH:mm'), // Format timestamp as desired
+                        y: data.pm25Value, // Use the appropriate field from your data
+                        });
+                }
+                
             });
-            // Iterate through the results
-            querySnapshot.forEach((doc) => {
-            const data = doc.data();
-            // Process each document data as needed
-            console.log('Document data:', data);
-            });
+            console.log('Readings are', newDataPoints);
+
           } catch (error) {
             console.error('Error querying Firestore:', error);
           }
@@ -62,6 +72,7 @@ function ViewHistory(){
 
     return (<View>
         	<Text>This is the Air Quality measurements from the last 24 hours.</Text>
+            
             </View>)
 }
 
